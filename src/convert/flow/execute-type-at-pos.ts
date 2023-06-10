dee3781b4864009682bec0d5b6ab7247c4594b77
Andy Edwards <jedwards@fastmail.com>
2023-06-10 14:05:07 -0500

    fix: run flow-bin that's installed in the project...duh??

--- src/convert/flow/execute-type-at-pos.ts
+++ src/convert/flow/execute-type-at-pos.ts
@@ -1,5 +1,6 @@
 import childProcess from "child_process";
 import * as t from "@babel/types";
+import path from "path";
 
 /**
  * Actually executes `flow type-at-pos`. This will be called behind a throttle.
@@ -8,14 +9,21 @@ export async function executeFlowTypeAtPos(
   filePath: string,
   location: t.SourceLocation
 ): Promise<string> {
+  const { default: flowBin } = await import(
+    require.resolve("flow-bin", {
+      paths: [path.dirname(filePath)],
+    })
+  );
+  const rootDir = flowBin.replace(/\/node_modules.*/, "");
+  const relFilePath = path.relative(rootDir, filePath);
   const { line, column } = location.start;
-  const command = `$(yarn bin)/flow type-at-pos "${filePath}" ${line} ${
+  const command = `${flowBin} type-at-pos "${relFilePath}" ${line} ${
     column + 1
   } --json --from "typescriptify" --quiet`;
 
   // Actually run Flow...
   const stdout = await new Promise<string>((resolve, reject) => {
-    childProcess.exec(command, (error, stdout) => {
+    childProcess.exec(command, { cwd: rootDir }, (error, stdout) => {
       if (error) {
         reject(error);
       } else {
